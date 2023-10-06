f610f574005a04a440f8ce094db53d8e8e1ae16a
Kristoffer Myhre <112164718+Caplix@users.noreply.github.com>
2023-10-06 07:28:16 +0200

    changed user-auth from oop to function based and made small changes to error-handeling

--- src/js/user-authentication.js
+++ src/js/user-authentication.js
@@ -1,87 +1,83 @@
 import { getSingleElements } from "./dom.js";
 import { nameRegex, emailRegex, passwordRegex } from "./validation.js";
+
 const BASE_URL = `https://api.noroff.dev/api/v1/`;
 const NEW_USER_URL = `social/auth/register`;
 const LOGIN_URL = `social/auth/login`;
 
-class NewUser {
-  constructor() {
-    const signUpForm = getSingleElements("#signup-form");
-    const newNameInput = getSingleElements("#full-name");
-    const newEmailInput = getSingleElements("#new-email");
-    const newPasswordInput = getSingleElements("#new-password");
-    const signUpBtn = getSingleElements(".signup-btn");
-    const agreedprivacy = getSingleElements("#agreed-privacy");
+const signUpForm = getSingleElements("#signup-form");
+const newNameInput = getSingleElements("#full-name");
+const newEmailInput = getSingleElements("#new-email");
+const newPasswordInput = getSingleElements("#new-password");
+const signUpBtn = getSingleElements(".signup-btn");
+const agreedprivacy = getSingleElements("#agreed-privacy");
 
-    signUpBtn.disabled = true;
-    agreedprivacy.addEventListener("change", function () {
-      signUpBtn.disabled = !agreedprivacy.checked;
-    });
+signUpBtn.disabled = true;
+agreedprivacy.addEventListener("change", function () {
+  signUpBtn.disabled = !agreedprivacy.checked;
+});
 
-    signUpForm.addEventListener("submit", (e) => {
-      e.preventDefault();
-      const newNameInputValue = newNameInput.value.trim();
-      const newEmailInputValue = newEmailInput.value.trim();
-      const newPasswordInputValue = newPasswordInput.value.trim();
-      console.log(newNameInputValue);
-      console.log(newEmailInputValue);
-      console.log(newPasswordInputValue);
-      const invalidAlert = getSingleElements(".alert-container");
+signUpForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const newNameInputValue = newNameInput.value.trim();
+  const newEmailInputValue = newEmailInput.value.trim();
+  const newPasswordInputValue = newPasswordInput.value.trim();
+  console.log(newNameInputValue);
+  console.log(newEmailInputValue);
+  console.log(newPasswordInputValue);
+  const invalidAlert = getSingleElements(".alert-container");
 
-      if (!nameRegex.test(newNameInputValue)) {
-        invalidAlert.innerHTML = `<p>Full name is invalid. Please enter a valid name</p>`;
-        return;
-      }
-      if (!emailRegex.test(newEmailInputValue)) {
-        invalidAlert.innerHTML = `<p>Email is invalid. Please enter a valid Email</p>`;
-        return;
-      }
-      if (!passwordRegex.test(newPasswordInputValue)) {
-        invalidAlert.innerHTML = `<p>Password is invalid. Please enter a valid password</p>`;
-        return;
-      } else {
-        invalidAlert.innerHTML = ``;
-      }
-      console.log("All inputs are valid.");
-
-
-
-      if(nameRegex.test(newNameInputValue)&&(emailRegex.test(newEmailInputValue)&&(passwordRegex.test(newPasswordInputValue)))){
-        this.registerUser(newNameInputValue,newEmailInputValue,newPasswordInputValue)
-        window.location.href="./index.html"
-      }
-
-
-    });
+  if (!nameRegex.test(newNameInputValue)) {
+    invalidAlert.innerHTML = `<p>Full name is invalid. Please enter a valid name.</p>`;
+    return;
   }
-//km.stud.noroff.no
-//banan1234
+  if (!emailRegex.test(newEmailInputValue)) {
+    invalidAlert.innerHTML = `<p>Email is invalid. Please enter a valid Email. Must use stud.noroff.no/noroff.no</p>`;
+    return;
+  }
+  if (!passwordRegex.test(newPasswordInputValue)) {
+    invalidAlert.innerHTML = `<p>Password is invalid. Please enter a valid password (must contain 8 characters)</p>`;
+    return;
+  } else {
+    invalidAlert.innerHTML = ``;
+  }
+  console.log("All inputs are valid.");
 
+  try {
+    await registerUser(newNameInputValue, newEmailInputValue, newPasswordInputValue);
+    window.location.href = "./index.html";
+  } catch (error) {
+    console.error(error);
+  }
+});
 
-  async registerUser(name, email, password) {
-    console.log(name, email, password)
-    try {
-      const res = await fetch(BASE_URL + NEW_USER_URL, {
-        method: "POST",
-        body: JSON.stringify({
-          name: name,
-          email: email,
-          password: password,
-        }),
-        headers: { "content-type": "application/json; charset=UTF-8" },
-      });
-      console.log(res.status);
-      //gjør denne erroren bedre
-      if (res.status !== 201) {
-        console.log("profile already exists");
-      } else {
-        let data = res.json();
-        console.log(data);
-      }
-    } catch (error) {}
+async function registerUser(name, email, password) {
+  console.log(name, email, password);
+  try {
+    const res = await fetch(BASE_URL + NEW_USER_URL, {
+      method: "POST",
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        password: password,
+      }),
+      headers: { "content-type": "application/json; charset=UTF-8" },
+    });
+
+    if (res.status !== 201) {
+      let notNewProfile = getSingleElements(".alert-container")
+      notNewProfile.innerHTML="Registration failed: Profile already exists or other error occurred";
+      throw new Error
+    }
+
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
 }
-const testUser = new NewUser();
+
 /*  testUser.registerUser()     */
 
 
