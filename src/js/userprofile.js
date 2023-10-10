f7df129d20963827c566b304ec33118b32fdf9c3
jmjjmj994 <jonathan.mjos@outlook.com>
2023-10-10 13:55:17 +0200

    Users can now change avatar by pasting a image url in a modal. Gonna add posibility to change banner aswell

--- src/js/userprofile.js
+++ src/js/userprofile.js
@@ -1,17 +1,58 @@
 import { getSingleElements } from "./dom.js";
-/* import { getSingleElements } from "./dom.js";
 const BASE_URL = `https://api.noroff.dev/api/v1/`;
+const userData = JSON.parse(localStorage.getItem("user"));
+function renderProfile() {
+  const username = getSingleElements(".profile-main__user--name");
+  const profileImg = getSingleElements(".profile-main__user--img");
+  /*   const profileData = JSON.parse(localStorage.getItem("user")); */
 
-async function createPost(title, body, tags, media) {
-  console.log(title, body, tags, media);
+  const user = {
+    name: userData.name,
+    email: userData.email,
+    avatar: userData.avatar,
+    banner: userData.banner,
+  };
+  console.log(user.avatar);
+  username.textContent = user.name;
+  profileImg.src = user.banner;
+  if (!userData.banner) {
+    profileImg.src =
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png ";
+  } else {
+    profileImg.src = user.banner;
+  }
+
+  showUserDetails(user.name, user.email, user.banner);
+}
+
+function showUserDetails(name, email, banner) {
+  const btnView = getSingleElements(".btn-view");
+  const userModal = getSingleElements(".show-user-wrapper");
+  const userName = getSingleElements(".show-user-information__username");
+  const userEmail = getSingleElements(".show-user-information__useremail");
+  const profileImage = getSingleElements(".show-user-information__image");
+  btnView.addEventListener("click", (e) => {
+    userModal.style.visibility = "visible";
+    userName.textContent = name;
+    userEmail.textContent = email;
+    if (!banner) {
+      profileImage.src =
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png ";
+    } else {
+      profileImage.src = banner;
+    }
+  });
+}
+
+// change profile picture //
+
+async function changeUserImage(imgUrl, username) {
+  console.log(imgUrl, username);
   try {
-    const res = await fetch(BASE_URL + `social/posts`, {
-      method: "POST",
+    const res = await fetch(BASE_URL + `social/profiles/${username}/media`, {
+      method: "PUT",
       body: JSON.stringify({
-        title: title,
-        body: body,
-        tags: tags,
-        media: media,
+        banner: imgUrl,
       }),
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
@@ -19,113 +60,56 @@ async function createPost(title, body, tags, media) {
       },
     });
     const data = await res.json();
-    console.log(data);
+
+    userData.banner = data.banner;
+    localStorage.setItem("user", JSON.stringify(userData));
   } catch (error) {}
 }
 
-function handleNavbarClick() {
-  const mainNavbarBtn = getSingleElements("#main-navbar-btn");
-  const subNavbarBtn = getSingleElements("#sub-navbar-btn");
-  const modalFormWrapper = getSingleElements(".create-post-wrapper");
-  const modalForm = getSingleElements(".create-post__form");
-  mainNavbarBtn.onclick = () => showPostModal();
-subNavbarBtn.onclick = () => showPostModal();
-  function showPostModal() {
-    modalFormWrapper.classList.add("active");
-    modalFormWrapper.classList.remove("hidden");
-  }
-
-  function hideModals() {
-    modalFormWrapper.addEventListener("click", (e) => {
-      if (e.currentTarget === modalForm || modalForm.contains(e.target)) {
-        console.log("ok"); //error
-      } else if (
-        e.currentTarget !== modalForm ||
-        !modalForm.contains(e.target)
-      ) {
-        modalFormWrapper.classList.remove("active");
-        modalFormWrapper.classList.add("hidden");
-      }
-    });
-  }
-  hideModals();
+function getCurrentUsername() {
+  const profileImage = getSingleElements(
+    ".change-user-img__form-current-img--image"
+  );
+  const image = userData.banner;
+  !image
+    ? (profileImage.src =
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png")
+    : (profileImage.src = image);
+  const username = userData.name;
+  return username;
 }
+async function changeUserInput() {
+  const imageForm = getSingleElements("#change-user-img__form");
+  const imageInput = getSingleElements("#change-user-img__form--input");
+  const submitBtn = getSingleElements(".btn-change-img");
 
-function handleInput() {
- const modalFormWrapper = getSingleElements(".create-post-wrapper");
-  const titleInput = getSingleElements("#create-post__title");
-  const mediaInput = getSingleElements("#create-post__image");
-  const tagsInput = getSingleElements("#create-post__tags");
-  const bodyInput = getSingleElements("#create-post__content");
-  const submitBtn = getSingleElements(".btn-create__post");
-  submitBtn.addEventListener("click", (e) => {
+  submitBtn.addEventListener("click", async (e) => {
+    const username = getCurrentUsername();
     e.preventDefault();
-    const titleInputVal = titleInput.value.trim();
-    const mediaInputVal = mediaInput.value.trim();
-    const tagsInputVal = tagsInput.value.trim().split(",");
-    const bodyInputVal = bodyInput.value.trim();
-
-    if (titleInputVal.length > 0) {
-      createPost(titleInputVal, bodyInputVal, tagsInputVal, mediaInputVal);
-      titleInput.value = "";
-      mediaInput.value = "";
-      tagsInput.value = "";
-      bodyInput.value = "";
-      modalFormWrapper.classList.remove("active");
-      modalFormWrapper.classList.add("hidden");
+    
+    const imageInputValue = imageInput.value.trim();
+    if (!imageInputValue) {
+      console.log("please enter a valid image url")
     } else {
-      console.log("please fill in title");
+      try {
+        await changeUserImage(imageInputValue, username);
+        imageInput.value = "";
+        window.location.reload()
+      } catch (error) {
+        //error
+      }
     }
+
   });
 }
- */
-function renderProfile() {
-  const username = getSingleElements(".profile-main__user--name");
-  const profileImg = getSingleElements(".profile-main__user--img");
-  const profileData = JSON.parse(localStorage.getItem("user"));
- 
-  const user = {
-    name: profileData.name,
-    email: profileData.email,
-    avatar: profileData.avatar,
-    banner: profileData.banner,
-  };
 
-  username.textContent = user.name;
-  !user.avatar
-    ? (profileImg.src =
-        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png ")
-    : (profileImg.src = user.avatar);
-
-  showUserDetails(user.name, user.email, user.avatar);
-}
-
-function showUserDetails(name, email, avatar) {
-  const btnView = getSingleElements(".btn-view");
-  const userModal = getSingleElements(".show-user-wrapper");
-  const userName = getSingleElements(".show-user-information__username");
-  const userEmail = getSingleElements(".show-user-information__useremail");
-  const profileImage = getSingleElements(".show-user-information__image");
-
-  btnView.addEventListener("click", (e) => {
-    userModal.style.visibility = "visible";
-    userName.textContent = name;
-    userEmail.textContent = email;
-    !avatar
-      ? (profileImage.src =
-          "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png ")
-      : (profileImage.src = avatar);
-    const data = JSON.parse(localStorage.getItem("user"));
-  });
-}
+// change profile picture //
 
 (() => {
-/*   handleNavbarClick();
-  handleInput(); */
+  changeUserInput();
   renderProfile();
+  getCurrentUsername();
 })();
-
-
 
 
 
@@ -139,12 +123,11 @@ async function singleEntry(param) {
       },
     });
     const data = await response.json();
-    console.log(data);
+    /* console.log(data); */
   } catch (error) {}
 }
 
-singleEntry("norofftest123"); 
-
+singleEntry("norofftest123");
 
 /* 
 const ALL_PROFILES_URL = `social/profiles`;
