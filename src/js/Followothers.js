5d2c20cbb32f71350e741fb1d24aee8f1b2a5722
Bilal1998a <101061169+Bilal1998a@users.noreply.github.com>
2023-10-14 11:03:10 +0200

    follow function/localstorage

--- src/js/Followothers.js
+++ src/js/Followothers.js
@@ -1,36 +1,40 @@
-
-
-
-
-
 const followButton = document.querySelector('.btn-follow');
 const followerCountElement = document.querySelector('[data-followers-stat]');
 
-let followerCount = parseInt(followerCountElement.textContent);
-let isFollowing = false;
+
+let followerCount = parseInt(localStorage.getItem('followerCount')) || parseInt(followerCountElement.textContent);
+let isFollowing = localStorage.getItem('isFollowing') === 'true' || false;
+
+
+function updateUI() {
+  followerCountElement.textContent = followerCount;
+  followButton.textContent = isFollowing ? 'Following' : 'Follow';
+}
+
+
+updateUI();
 
 function handleFollowButtonClick() {
   if (isFollowing) {
-   
+    
     followerCount--;
   } else {
-  
+    
     followerCount++;
   }
+
+  
+  isFollowing = !isFollowing;
+
 
   localStorage.setItem('followerCount', followerCount);
   localStorage.setItem('isFollowing', isFollowing);
 
-  isFollowing = !isFollowing;
 
-  
-  followerCountElement.textContent = followerCount;
-
-  followButton.textContent = isFollowing ? 'Following' : 'Follow';
+  updateUI();
 }
 
 followButton.addEventListener('click', handleFollowButtonClick);
-
 
 
 
