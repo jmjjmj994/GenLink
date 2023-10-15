ea7247abf99f71062c30e7dfc7e1ea463e81de30
Kristoffer Myhre <112164718+Caplix@users.noreply.github.com>
2023-10-15 19:19:58 +0200

    added ability to post and view comments on post specific page

--- src/js/post-specific-page.js
+++ src/js/post-specific-page.js
@@ -1,91 +1,112 @@
-
 import { getSingleElements } from "./dom.js";
+
 const BASE_URL = `https://api.noroff.dev/api/v1/`;
 const POST_PARAM = `social/posts?`;
 
-const queryString = document.location.search;
-const params = new URLSearchParams(queryString);
-const id = params.get("id");
-console.log(id)
+function initialize() {
+  const queryString = document.location.search;
+  const params = new URLSearchParams(queryString);
+  const id = params.get("id");
+  console.log(id);
 
+  specificPost(id);
+}
 
 async function specificPost(id) {
-    console.log(id)
-    
-    try {
-      const response = await fetch(
-        
-        BASE_URL + `social/posts/${id}`,
-        {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-            "content-type": "application/json; charset=UTF-8",
-          },
-        }
-      );
-      const data = await response.json();
-      const specificPostContainer = getSingleElements(".post-details-container")
-      specificPostContainer.innerHTML += `
-      <div class ="post-details-container">
-      <img src=${data.media}>
+  try {
+    const data = await fetchPostData(id);
+
+    const specificPostContainer = getSingleElements(".post-details-container");
+    specificPostContainer.innerHTML = buildPostHTML(data);
+
+    const commentSection = data.comments.map((comment) => ({
+      title: comment.body,
+      user: comment.owner,
+    }));
+
+    const postCommentSection = getSingleElements("#comment-section");
+    postCommentSection.innerHTML = buildCommentSectionHTML(commentSection);
+
+    const commentForm = getSingleElements("#comment-form");
+    const commentInput = getSingleElements("#new-comment");
+
+    commentForm.addEventListener("submit", async (event) => {
+      event.preventDefault();
+      const userComment = commentInput.value;
+      await createNewComment(id, userComment);
+      specificPost(id);
+      
+    });
+  } catch (err) {
+    console.error(err.status);
+  }
+}
+
+async function fetchPostData(id) {
+  const response = await fetch(
+    BASE_URL + `social/posts/${id}?_comments=true`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "content-type": "application/json; charset=UTF-8",
+      },
+    }
+  );
+  return response.json();
+}
+
+function buildPostHTML(data) {
+  return `
+    <div class="post-details-container">
+      <img src="${data.media}">
       <h1>${data.title}</h1>
       <h2>${data.body}</h2>
-      <div class ="post-reations">
-      <div class ="post-likes">👍${data._count.reactions}</div>
-      <div class ="post-comments"><i class="fa-regular fa-comment"></i>${data._count.comments}</div>
-      
-    
-      <form id="comment-form">
-      <label for="new-comment">Post a comment</label>
-      <input type="text" id="new-comment" name="new-comment">
-      <button class="new-comment-button" type="submit">Submit</button>
-      </form>
+      <div class="post-reations">
+        <div class="post-likes">👍${data._count.reactions}</div>
+        <div class="post-comments"><i class="fa-regular fa-comment"></i>${data._count.comments}</div>
+        
+        <form id="comment-form">
+          <label for="new-comment">Post a comment</label>
+          <input type="text" id="new-comment" name="new-comment">
+          <button class="new-comment-button" type="submit">Submit</button>
+        </form>
 
-      </div>`
-      const commentInput = getSingleElements("#new-comment");
+        <div class="comment-section" id="comment-section"></div>
+      </div>
+    </div>
+  `;
+}
 
-      const commentForm = getSingleElements(".new-comment-button");
-              commentForm.addEventListener("click", async (event) => {
-              event.preventDefault();
-              const userComment = commentInput.value.trim();
+function buildCommentSectionHTML(commentSection) {
+  return commentSection
+    .map((comment) => `
+      <div class="comment-container">
+        <p class="comment-user-name">${comment.user}</p>
+        <p class="comment-content">${comment.title}</p>
+      </div>
+    `)
+    .join("");
+}
 
-              createNewComment(userComment);
-  
-              commentInput.value = '';
-          });
-
-
-      console.log(data)
-    
-    } catch (err) {
-        console.error(err);
+async function createNewComment(id, comment) {
+  try {
+    const response = await fetch(
+      BASE_URL + `social/posts/${id}/comment`,
+      {
+        method: "POST",
+        body: JSON.stringify({ body: comment }),
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          "content-type": "application/json",
+        },
       }
-      
-    }
-    async function createNewComment(comment) {
-      console.log(comment)
-      console.log(id)
-      try {
-          const response = await fetch(
-            BASE_URL + `social/posts/${id}/comment`,
-              {
-                method: "POST",
-                body: JSON.stringify({comment: comment}),
-                   headers: {
-                      Authorization: `Bearer ${localStorage.getItem("token")}`,
-                      "content-type": "application/json; charset=UTF-8",
-                  }, 
-
-              }
-          );
-              console.log(response)
-
-
-      } catch (err) {
-        const errorResponse = await response.text();
-        console.error("Error posting comment:", errorResponse);
-
-      }
+    );
+    console.log(response);
+  } catch (err) {
+    const errorResponse = await response.text();
+    console.error("Error posting comment:", errorResponse);
   }
-  specificPost(parseInt(id));
+}
+
+initialize();
