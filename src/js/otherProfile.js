9a55a6bad7922ccc69618d362e5474842ad88c08
Bilal1998a <101061169+Bilal1998a@users.noreply.github.com>
2023-10-16 22:23:38 +0200

    follow function now complete with endpoints

--- src/js/otherProfile.js
+++ src/js/otherProfile.js
@@ -1,63 +1,142 @@
 const queryString = document.location.search;
+
 const params = new URLSearchParams(queryString);
+
 const username = params.get("name");
+
 const BASE_URL = `https://api.noroff.dev/api/v1/`;
 
-async function getUserData(name, callback) {
-  try {
-    const res = await fetch(BASE_URL + `social/profiles/${name}`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-        "content-type": "application/json; charset=UTF-8",
-      },
-    });
-    const data = await res.json();
-    const username = data.name;
-    const avatar = data.avatar;
-    const followers = data._count.followers
-    const following = data._count.following
-    const posts = data._count.posts
+let GlobalFollowers = []
 
-   callback(username, avatar, followers, following, posts)
-   
+let followApiEndpoint = BASE_URL + `social/profiles/${username}/`
+
+const myUsername = JSON.parse(localStorage.getItem("user")).name
+
+
+async function getUserData(name, callback) {
+
+  console.log(callback, "callback")
+
+  try {
+
+    const res = await fetch(BASE_URL + `social/profiles/${username}?_followers=true`, {
+
+      method: "GET",
+
+      headers: {
+
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+
+        "content-type": "application/json; charset=UTF-8",
+
+      },
+
+    });
+
+    const data = await res.json();
+
+
+    const fetchedName = data.name; 
+
+    const avatar = data.avatar;
+
+    const followersCount = data._count.followers;
+
+    const following = data._count.following;
+
+    const posts = data._count.posts;
+
+    const followers = data.followers
+
+    GlobalFollowers = followers
+
+    callback(fetchedName, avatar, followersCount, following, posts, followers); 
+
+    console.log(data, "getuserprofile")
+
   } catch (error) {
- 
+
+    console.error("Error fetching user data:", error);
+
   }
 }
 
 getUserData(username, renderHTML);
 
+function renderHTML(name, avatar, followersCount, following, posts) {
 
-function renderHTML(name, avatar, followers, following, posts){
-const userImg = document.querySelector(".profile-main__user--img")
-userImg.src = avatar
-const username = document.querySelector(".profile-main__user--name")
-username.textContent = name
-const userFollowers = document.querySelector(".followers-count")
-const userFollowing = document.querySelector(".follow-count")
+  const userImg = document.querySelector(".profile-main__user--img");
+  const usernameElement = document.querySelector(".profile-main__user--name");
+  const userFollowers = document.querySelector(".followers-count");
+  const userFollowing = document.querySelector(".follow-count");
+  const userPosts = document.querySelector(".posts-count");
 
-const userPosts = document.querySelector(".posts-count")
+  usernameElement.textContent = name;
+  userFollowers.textContent = followersCount;
+  userFollowing.textContent = following;
+  userPosts.textContent = posts;
+  userImg.src = avatar;
+  followApiEndpoint = BASE_URL + `social/profiles/${username}/`
 
-if(followers > 0) {
-    userFollowers.textContent = followers
-} else {
-    userFollowers.textContent = 0
+  const isFollower = GlobalFollowers.some(profile => profile.name === myUsername)
+
+ if (isFollower) {
+  followBtn.innerText = "Unfollow"
+  followApiEndpoint += "unfollow"
+ } else {
+  followBtn.innerText = "Follow"
+  followApiEndpoint += "follow"
+ }
 }
-if(following > 0){
-    userFollowing.textContent = following
-} else {
-    userFollowing.textContent = 0
+
+async function LetFollow(username) {
+
+  try {
+
+    const res = await fetch(BASE_URL + `social/profiles/${username}?_followers=true`, {
+
+      method: "PUT",
+      headers: {
+
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+
+      },
+
+    });
+
+    console.log(res, "response")
+
+  }
+
+catch (error) {
+
+  console.log(error, "THIS IS AN ERROR")
+
 }
-if(posts > 0) {
-    userPosts.textContent = posts
-} else {
-    userPosts.textContent = 0
 }
 
 
+const followBtn = document.querySelector(".btn-follow");
+followBtn.addEventListener("click",()=>{
+  followToggle()
+})
 
+async function followToggle() {
+  console.log(myUsername, "myUserName")
+  console.log(GlobalFollowers, "GlobalFollowers")
 
-console.log(userFollowers, userFollowing, userPosts)
+  try {
+    const res = await fetch(followApiEndpoint, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+       
+      },
+    });
+  console.log(res)
+getUserData(username, renderHTML)
+  } catch (error) {}
+
 
 }
+
